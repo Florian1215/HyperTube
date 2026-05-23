8419e72760c5d9372056141eefdcd5ee3bf4278a
Jules <jbjulesbernard3@gmail.com>
2026-05-23 17:11:34 +0200

    feat: implement HLS conversion

--- services/torrent-stream/internal/stream/handler.go
+++ services/torrent-stream/internal/stream/handler.go
@@ -18,7 +18,7 @@ func (s *StreamHandler) InitStream(w http.ResponseWriter, r *http.Request) {
 	videoPath := "/data/videos/" + id
 	torrentPath := "/data/torrents/" + id + "/rubber.mp4"
 	os.MkdirAll(videoPath, 0755) //TODO only do if folder doesnt exist, and watchout for folder existing but not with a complete stream.
-	err := transcode.TranscodeHLS(torrentPath, videoPath+"/index.m3u8")
+	err := transcode.ConvertHLS(torrentPath, videoPath)
 	if err != nil {
 		http.Error(w, "failed to start stream", http.StatusInternalServerError)
 		log.Printf("failed to start stream: %v", err)
@@ -30,7 +30,7 @@ func (s *StreamHandler) InitStream(w http.ResponseWriter, r *http.Request) {
 
 func (s *StreamHandler) GetIndex(w http.ResponseWriter, r *http.Request) {
 	id := r.PathValue("id")
-	videoPath := "/data/videos/" + id 
+	videoPath := "/data/videos/" + id
 	w.Header().Set("Content-Type", "application/vnd.apple.mpegurl")
 	if bytes, err := os.ReadFile(videoPath + "/index.m3u8"); err != nil {
 		http.Error(w, "failed to read index file", http.StatusInternalServerError)
@@ -43,7 +43,7 @@ func (s *StreamHandler) GetIndex(w http.ResponseWriter, r *http.Request) {
 
 func (s *StreamHandler) GetSegment(w http.ResponseWriter, r *http.Request) {
 	id := r.PathValue("id")
-	prefix := "/data/videos/" + id 
+	prefix := "/data/videos/" + id
 	segment := r.PathValue("segment")
 	w.Header().Set("Content-Type", "video/mp2t")
 	if bytes, err := os.ReadFile(prefix + segment); err != nil {
