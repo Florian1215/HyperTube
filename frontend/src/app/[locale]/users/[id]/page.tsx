4a41c41325b989fac3f037a0b9d8068a642ce7f5
florian <florian.guiramand@protonmail.com>
2026-06-05 19:43:15 +0200

    feat: make user/{id} page

--- frontend/src/app/[locale]/users/[id]/page.tsx
+++ frontend/src/app/[locale]/users/[id]/page.tsx
@@ -1,4 +1,32 @@
-// todo remove ?
+"use client";
+
+import {useParams} from "next/navigation";
+import ProfilePage, {tTab} from "@/components/profile/ProfilePage";
+import MovieHistoryTab from "@/components/profile/HistoryTab";
+import CommentsTab from "@/components/profile/CommentTab";
+import {useUser} from "@/api/users";
+import React, {useEffect, useState} from "react";
+import {useHandleError} from "@/hooks/useApiQuery";
+import {ApiError} from "@/api/errors";
+
 export default function Page() {
-    return <h1>search</h1>;
+    const params = useParams();
+    const userId = params.id as string;
+    const [errorNode, setErrorNode] = useState<React.ReactNode>(null);
+    const tabs: tTab = [{name: "history", comp: MovieHistoryTab}, {name: "comments", comp: CommentsTab}];
+    const handleError = useHandleError();
+    const {data, error} = useUser(userId);
+
+    useEffect(() => {
+        if (error) {
+            const node = handleError(error as ApiError, "Film");
+            // eslint-disable-next-line react-hooks/set-state-in-effect
+            setErrorNode(node);
+        }
+    }, [error, handleError]);
+
+    if (errorNode || !data)
+        return (errorNode);
+
+    return <ProfilePage user={data.data} tabs={tabs} />;
 }
