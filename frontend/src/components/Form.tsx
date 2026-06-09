f11994093fe30d7848ae0275c4adf1a22af7064b
florian <florian.guiramand@protonmail.com>
2026-06-09 20:11:38 +0200

    refactor: update Form component to use specific field types

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -7,8 +7,9 @@ import {tResponse} from "@/api/client";
 import {iUserToken} from "@/types/user";
 
 type formType = "auth" | "update" |  "signin" | "register" | "reset-password";
+type fieldType = "email" | "login" | "first_name" |  "last_name" | "username" | "password";
 
-export default function Form({formType, request, handleRequest, t, fields, handleForgotPassword}: {formType: formType, request: (locale: string, data: string[]) => Promise<tResponse<iUserToken>>, handleRequest: (data: tResponse<iUserToken>) => void, t: (key: string) => string, fields: string[], handleForgotPassword?: () => void}) {
+export default function Form({formType, request, handleRequest, t, fields, handleForgotPassword}: {formType: formType, request: (locale: string, data: string[]) => Promise<tResponse<iUserToken>>, handleRequest: (data: tResponse<iUserToken>) => void, t: (key: string) => string, fields: fieldType[], handleForgotPassword?: () => void}) {
     const [fieldsValue, setFieldsValue] = useState<string[]>(Array(fields.length).fill(""));
     const [errors, setErrors] = useState<Record<string, string>>({});
     const [disableBtn, setDisableBtn] = useState(false);
@@ -22,7 +23,7 @@ export default function Form({formType, request, handleRequest, t, fields, handl
             fieldRefs.current[focusedIndex]?.focus();
     }, [focusedIndex]);
 
-    const getId = (field: string | number) => (typeof field  === "string" ? field : fields[field]) + "-" + formType;
+    const getId = (field: fieldType | number) => (typeof field  === "number" ? fields[field] : field) + "-" + formType;
     const hasError = (error: Record<string, string>) => Object.keys(error).length > 0 && Object.values(error).some((v) => !!v);
 
     const newSetterError = (value: Record<string, string>) => {
@@ -89,7 +90,6 @@ export default function Form({formType, request, handleRequest, t, fields, handl
                     if (!focusIsSet) {
                         focusIsSet = true;
                         setFocusedIndex(idx);
-                        console.log("FOCUS", fieldsValue, requiredErrors, errors, idx);
                     }
                 }
             })
@@ -99,17 +99,16 @@ export default function Form({formType, request, handleRequest, t, fields, handl
                 if (data) {
                     handleRequest(data);
                     setFieldsValue(Array(fields.length).fill(""));
-                } else
-                    console.log("ERROR", errors);
+                }
             })
             fieldRefs.current[focusedIndex]?.blur();
             setFocusedIndex(-1);
         }
     };
 
-    const RenderInput = (type: string, idx: number, className?: string) =>
+    const RenderInput = (type: fieldType, idx: number, className?: string) =>
         <Input key={idx} id={getId(type)} type={type === "password" ? "password" : "text"} placeholder={t(type)}
-               value={fieldsValue[idx]} idx={idx} onChange={setFieldsValue} className={className}
+               value={fieldsValue[idx]} idx={idx} onChange={setFieldsValue} className={className + ((formType === "register" && (type === "username" || type === "password")) ? " " : "")}
                requestErrorMessage={errors[getId(type)]} setErrorsMessage={newSetterError} ref={(el: HTMLInputElement) => {fieldRefs.current[idx] = el;}}
                onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => handleKeyDown(e, idx)}></Input>;
 
