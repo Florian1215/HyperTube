9250ec1d575c241d26ebf52006d21a709d0a34c2
florian <florian.guiramand@protonmail.com>
2026-06-11 16:49:55 +0200

    feat: handle loading state before opening reset password modal and add login check

--- frontend/src/app/[locale]/reset-password/page.tsx
+++ frontend/src/app/[locale]/reset-password/page.tsx
@@ -6,6 +6,7 @@ import {useSearchParams} from "next/navigation";
 import {useNotification} from "@/context/NotificationContext";
 import {useModal} from "@/context/ModalContext";
 import {useEffect} from "react";
+import {useAuth} from "@/context/AuthContext";
 
 export default function Page() {
     const searchParams = useSearchParams();
@@ -13,17 +14,21 @@ export default function Page() {
     const {addNotification} = useNotification();
     const router = useRouter();
     const {openModal} = useModal();
+    const {user, loading} = useAuth();
     const tError = useTranslations("notifications.error");
 
 
     useEffect(() => {
-        if (token)
-            openModal({type: "set-new-password", token: token});
-        else
-            addNotification(tError("missingToken"), "error");
-
-        setTimeout(() => {
-            router.replace("/");
-        }, 0);
-    }, []);
+        if (!loading) {
+            if (!user) {
+                if (token)
+                    openModal({type: "set-new-password", token: token});
+                else
+                    addNotification(tError("missingToken"), "error");
+            }
+            setTimeout(() => {
+                router.replace("/");
+            }, 0);
+        }
+    }, [loading]);
 }
