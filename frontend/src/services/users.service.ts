c3dfd1d77445d71d543aa3113baf0c4e5b1604be
florian <florian.guiramand@protonmail.com>
2026-06-14 22:04:24 +0200

    update: implement avatar update functionality in AvatarSection

--- frontend/src/services/users.service.ts
+++ frontend/src/services/users.service.ts
@@ -10,14 +10,18 @@ function getUser(userId: string, locale: string) {
 export function patchUser(locale: string, data: string[], userId?: string) {
     const updateData: Record<string, string> = {};
 
-    if (data[0])
-        updateData["email"] = data[0].trim();
-    else if (data[1])
-        updateData["first_name"] = data[1].trim();
-    else if (data[2])
-        updateData["last_name"] = data[2].trim();
-    else if (data[3])
-        updateData["username"] = data[3].trim();
+    if (data.length === 2) {
+        updateData[data[0]] = data[1];
+    } else {
+        if (data[0])
+            updateData["email"] = data[0].trim();
+        else if (data[1])
+            updateData["first_name"] = data[1].trim();
+        else if (data[2])
+            updateData["last_name"] = data[2].trim();
+        else if (data[3])
+            updateData["username"] = data[3].trim();
+    }
     return apiClient<tResponse<iUserToken>>(`/users/${userId}`, locale, {method: "PATCH", body: JSON.stringify(updateData)});
 }
 
