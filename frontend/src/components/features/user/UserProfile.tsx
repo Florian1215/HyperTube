e516669b07de70313c5119dc3d122dceaca61529
florian <florian.guiramand@protonmail.com>
2026-06-21 16:52:37 +0200

    fix: translation system and remake 404 page handling

--- frontend/src/components/features/user/UserProfile.tsx
+++ frontend/src/components/features/user/UserProfile.tsx
@@ -34,6 +34,7 @@ export default function UserProfile({user, tabs, updateUserAction}: {user: iUser
         if (activeTab !== tabIdx)
             setActiveTab(tabIdx);
     }
+
     return (<div className="flex flex-col gap-6 sm:gap-10 xl:gap-17 px-2 md:px-4">
         <div className="flex items-center gap-4 justify-center">
             <ProfilePicture user={user} size={1}/>
