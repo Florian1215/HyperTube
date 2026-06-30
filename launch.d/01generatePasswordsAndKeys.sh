19a44c46f0726659da02e2b238dfb8a91b32e559
florian <florian.guiramand@protonmail.com>
2026-06-30 20:54:55 +0200

    feat: remake makefile and add script for generate password

---
+++ launch.d/01generatePasswordsAndKeys.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+ENV_DEFAULT=.env.exemple
+ENV_OUTPUT=srcs/.env
+
+generate_password() {
+    export LC_ALL=C
+    echo $(LC_CTYPE=C; tr -dc 'A-Za-z0-9!?%' < /dev/urandom | head -c 32)
+}
+
+generate_secret_key() {
+    python3 - << EOF
+import secrets
+length = 50
+chars = 'abcdefghijklmnopqrstuvwxyz0123456789%^&*(-_=+)'
+secret_key = ''.join(secrets.choice(chars) for i in range(length))
+print(secret_key)
+EOF
+}
+
+> "$ENV_OUTPUT"
+
+while IFS= read -r line; do
+    if [[ $line == "POSTGRES_PASSWORD"* ]]; then
+        variable=$(echo "$line" | cut -d '=' -f 1)
+        password=$(generate_password)
+        echo "$variable=\"$password\"" >> "$ENV_OUTPUT"
+    elif [[ $line == "JWT_SECRET"* ]]; then
+        variable=$(echo "$line" | cut -d '=' -f 1)
+        key=$(generate_secret_key)
+        echo "$variable=\"$key\"" >> "$ENV_OUTPUT"
+    elif [[ $line == "OAUTH_CLIENT_SECRET"* ]]; then
+        variable=$(echo "$line" | cut -d '=' -f 1)
+        key=$(generate_secret_key)
+        echo "$variable=\"$key\"" >> "$ENV_OUTPUT"
+    else
+        echo "$line" >> "$ENV_OUTPUT"
+    fi
+done < "$ENV_DEFAULT"
+
+exit 0
