a254d882d1d296878136da26723be4b44d9a3755
florian <florian.guiramand@protonmail.com>
2026-07-04 20:02:18 +0200

    feat: use MediaStream API

--- srcs/requirements/frontend/src/components/ui/VideoPlayer.tsx
+++ srcs/requirements/frontend/src/components/ui/VideoPlayer.tsx
@@ -55,6 +55,7 @@ export default function VideoPlayer({movie, src, color, setErrorAction, tAction}
     const delaySubtitle = useRef(0);
     const {addNotification} = useNotification();
     const tError = useTranslations("notifications.error");
+    const mediaStreamRef = useRef<MediaStream | null>(null);
 
     /* ---------------------------------------------------- INIT ---------------------------------------------------- */
     useEffect(() => {
@@ -115,6 +116,22 @@ export default function VideoPlayer({movie, src, color, setErrorAction, tAction}
         }
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [src]);
+
+    useEffect(() => {
+        const video = videoRef.current;
+        if (!video)
+            return;
+
+        const startCapture = async () => {
+            await video.play();
+
+            const stream = video.captureStream();
+            mediaStreamRef.current = stream;
+        };
+
+        startCapture();
+        return () => {mediaStreamRef.current?.getTracks().forEach(t => t.stop());};
+    }, []);
 
     /* ------------------------------------------------- PLAY PAUSE ------------------------------------------------- */
     useEffect(() => {
