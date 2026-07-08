86cc8904e521b306c2ba344dbf1d7b4708a460a5
florian <florian.guiramand@protonmail.com>
2026-07-08 16:50:11 +0200

    remove: useless config in proxy.ts

--- srcs/requirements/frontend/src/proxy.ts
+++ srcs/requirements/frontend/src/proxy.ts
@@ -2,5 +2,3 @@ import createMiddleware from "next-intl/middleware";
 import {routing} from "@/i18n/request";
 
 export default createMiddleware(routing);
-
-export const config = {matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)"};
