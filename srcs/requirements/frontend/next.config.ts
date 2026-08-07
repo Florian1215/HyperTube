f799e1d1a366d5d73f1171c22db796e5f7111e18
florian <florian.guiramand@protonmail.com>
2026-08-07 17:25:53 +0200

    fix: port for api call in next.config.ts

--- srcs/requirements/frontend/next.config.ts
+++ srcs/requirements/frontend/next.config.ts
@@ -28,7 +28,7 @@ const nextConfig: NextConfig = {
     return [
         {
             source: "/api/:path*",
-            destination: `http://api:8080/api/:path*`,
+            destination: `http://api:8794/api/:path*`,
         },
     ];
 },
