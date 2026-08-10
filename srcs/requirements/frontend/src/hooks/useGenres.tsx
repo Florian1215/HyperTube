ea54a071a6211f4451f93c8f073ca031b3dc6445
florian <florian.guiramand@protonmail.com>
2026-08-10 23:31:29 +0200

    refactor: only use local genre

--- srcs/requirements/frontend/src/hooks/useGenres.tsx
+++ srcs/requirements/frontend/src/hooks/useGenres.tsx
@@ -1,11 +1,22 @@
 "use client";
 
 import {useQuery} from "@tanstack/react-query";
-import {fetchGenres} from "@/services/genre.service";
+import {deGenres, enGenres, frGenres} from "@/types/genre";
 
 export default function useGenres(language: string) {
     return useQuery({
         queryKey: ["genres", language],
-        queryFn: () => fetchGenres(language),
+        queryFn: () => getFallbackGenres(language),
     });
+}
+
+function getFallbackGenres(language: string) {
+    switch (language) {
+        case "fr":
+            return { genres: frGenres };
+        case "de":
+            return { genres: deGenres };
+        default:
+            return { genres: enGenres };
+    }
 }
