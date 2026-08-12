89bd708b1dcab23e406083e8d8f29922b7906c8d
florian <florian.guiramand@protonmail.com>
2026-08-12 21:01:45 +0200

    feat: start people endpoint and db

---
+++ srcs/requirements/api/django/people/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class People(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=255)
