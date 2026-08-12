6fa56a6aaf15caf754d30f6823f3558b51fdaeb3
florian <florian.guiramand@protonmail.com>
2026-08-13 01:23:07 +0200

    fix: add session auth in 'DEFAULT_AUTHENTICATION_CLASSES'

--- srcs/requirements/api/django/config/settings.py
+++ srcs/requirements/api/django/config/settings.py
@@ -141,7 +141,8 @@ MAILERS = {
 # Rest framework
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
     ),
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
