1a32e5dfc3372e42059d96cbe29bab16fb6b35ed
florian <florian.guiramand@protonmail.com>
2026-08-13 01:22:42 +0200

    feat: add error msg

--- srcs/requirements/api/django/config/errors.py
+++ srcs/requirements/api/django/config/errors.py
@@ -1,1 +1,4 @@
-ERRORMSG_SEARCH_REQUIRED = "Search query is required."
+ERRORMSG_SEARCH_REQUIRED = 'Search query is required.'
+USERNAME_CONTAINS = 'Username can only contain letters, numbers and underscores.'
+USER_ALREADY_TAKEN = 'Username already taken.'
+MOVIE_NOT_FOUND = 'Movie not found.'
