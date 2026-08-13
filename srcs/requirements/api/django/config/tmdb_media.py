4884e73a509ac2b323d1d3631beaaac876d1e8cd
florian <florian.guiramand@protonmail.com>
2026-08-13 16:36:03 +0200

    fix: image size tmdb_media.py

--- srcs/requirements/api/django/config/tmdb_media.py
+++ srcs/requirements/api/django/config/tmdb_media.py
@@ -1,7 +1,22 @@
 from config.settings import TMDB_MEDIAS_URL
 
+from typing import Literal
 
-def tmdb_media(path, size='original'):
+TMDBImageSize = Literal[
+    'w45',
+    'w92',
+    'w154',
+    'w185',
+    'w300',
+    'w342',
+    'w500',
+    'w780',
+    'w1280',
+    'original'
+]
+
+
+def tmdb_media(path, size: TMDBImageSize):
     if path is None:
         return ''
-    return f'{TMDB_MEDIAS_URL}/original{path}'
+    return f'{TMDB_MEDIAS_URL}/{size}{path}'
